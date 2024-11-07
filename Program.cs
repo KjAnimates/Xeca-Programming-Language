4b835241b29b18dfa64ceacfeb672b0e23f18912
KjAnimates <kjhouser43@gmail.com>
2024-11-07 18:46:40 -0500

    Added CLI Features -i + -o.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,39 @@ public class Program
 {
     public static void Main(String[] Args)
     {
-        Console.WriteLine("Hello, world!");
+        // Check if there is an input
+        // <flag> <value>
+        if(Args.Length > 0) 
+        {
+            for(int i = 0; i < Args.Length; i++)
+            {
+                string arg = Args[i];
+                string ? Flag = null;
+                string ? Value = null;
+                
+                // Gets the flag and the value for the flag
+                if(i % 2 == 0)
+                {
+                    if (i + 1 < Args.Length)
+                    {
+                        Flag = Args[i];
+                        Value = Args[i + 1];
+                    }
+                }
+
+                if(Flag != null && Value != null)
+                {
+                    switch(Flag)
+                    {
+                        case "-i":
+                            Console.WriteLine("Input  = " + Value);
+                            break;
+                        case "-o":
+                            Console.WriteLine("Output = " + Value);
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
